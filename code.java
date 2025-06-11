cdd307a7ea43c9f370151182fbbf675a765c84ac
Aaryash-098 <aaryashberry@gmail.com>
2025-06-11 11:26:35 +0530

    ss

--- code.java
+++ code.java
@@ -4,11 +4,19 @@ class code {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         try {
-            System.out.println("enter 2 number");
+            System.out.println("Enter 2 numbers");
             float y = sc.nextFloat();
             float x = sc.nextFloat();
             float z =  x / y ;
-            System.out.println("The answer is ");
+            System.out.println("The answer is " + z);
+        }
+        catch(ArithmeticException ex) {
+            System.out.println("_______");
+            System.out.println(ex);
+        }
+        finally {
+            System.out.println("_______");
+            sc.close();
         }
     }
 }
